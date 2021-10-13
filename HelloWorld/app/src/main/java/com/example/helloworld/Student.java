17d2d66f6207ab5df8e42fcd8c0f344a7a47440e
Mihai Doinea <mihai.doinea@oracle.com>
2021-10-13 09:49:05 +0300

    HelloWorld

--- HelloWorld/app/src/main/java/com/example/helloworld/Student.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/Student.java
@@ -1,42 +1,6 @@
 package com.example.helloworld;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
-public class Student implements Parcelable {
+public class Student {
     String name;
     int varsta;
-
-    public Student(String name, int varsta) {
-        this.name = name;
-        this.varsta = varsta;
-    }
-
-    protected Student(Parcel in) {
-        name = in.readString();
-        varsta = in.readInt();
-    }
-
-    public static final Creator<Student> CREATOR = new Creator<Student>() {
-        @Override
-        public Student createFromParcel(Parcel in) {
-            return new Student(in);
-        }
-
-        @Override
-        public Student[] newArray(int size) {
-            return new Student[size];
-        }
-    };
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(name);
-        dest.writeInt(varsta);
-    }
 }
