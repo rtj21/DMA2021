563ebaf4bf9208010499064b9b6311937e0355d7
Mihai Doinea <mihai.doinea@oracle.com>
2021-11-29 18:00:25 +0200

    DAM08

--- DAM08/app/src/main/java/ro/csie/en/dam08/GreenFragment.java
+++ DAM08/app/src/main/java/ro/csie/en/dam08/GreenFragment.java
@@ -25,6 +25,7 @@ public class GreenFragment extends Fragment {
         super.onAttach(context);
         if(context instanceof OnGreenFragmentListener)
             greenFrgCallback = (OnGreenFragmentListener) context;
+        
     }
 
     @Nullable
